3c320b098fbbb4022f4ecdda263d34a41da62f0d
chenb <chenjpu@gmail.com>
2015-11-27 12:45:25 +0800

    init

---
+++ smartas-security/src/main/java/org/smartas/security/App.java
@@ -0,0 +1,13 @@
+package org.smartas.security;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
